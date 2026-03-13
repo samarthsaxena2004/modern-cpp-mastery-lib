88d4207f1c6e78a31b591fc36ed7d8e7d0d01195
Samarth Saxena <samarthsaxena001@gmail.com>
2026-03-13 19:44:00 +0530

    start FixedVector implementation - basic push_back and size

--- include/moderncppmastery/fixed_vector.hpp
+++ include/moderncppmastery/fixed_vector.hpp
@@ -1,7 +1,33 @@
 #pragma once
+#include <array>
+#include <stdexcept>
 
 namespace moderncppmastery {
 
-// TODO: implement FixedVector<T, N>
+// Fixed-size vector with dynamic size tracking
+template <typename T, size_t N>
+class FixedVector {
+public:
+    using value_type = T;
+    using size_type  = size_t;
+
+    FixedVector() = default;
+
+    void push_back(const T& value) {
+        if (size_ >= N)
+            throw std::overflow_error("FixedVector capacity exceeded");
+        data_[size_++] = value;
+    }
+
+    T& operator[](size_type index) { return data_[index]; }
+
+    size_type size()     const { return size_; }
+    size_type capacity() const { return N; }
+    bool      empty()    const { return size_ == 0; }
+
+private:
+    std::array<T, N> data_{};
+    size_type size_ = 0;
+};
 
 } // namespace moderncppmastery
