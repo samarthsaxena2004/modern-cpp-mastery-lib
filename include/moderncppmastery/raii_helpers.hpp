216e04095d3ce89cdf79da9876e4da0879949926
Samarth Saxena <samarthsaxena001@gmail.com>
2026-03-15 22:03:00 +0530

    add ScopedTimer class skeleton in raii_helpers

--- include/moderncppmastery/raii_helpers.hpp
+++ include/moderncppmastery/raii_helpers.hpp
@@ -1,7 +1,22 @@
 #pragma once
+#include <chrono>
+#include <iostream>
 
 namespace moderncppmastery {
 
-// TODO: implement ScopedTimer
+// RAII Timer - resource acquired in ctor, released in dtor
+class ScopedTimer {
+public:
+    explicit ScopedTimer(const char* name)
+        : name_(name), start_(std::chrono::high_resolution_clock::now()) {}
 
-} // namespace moderncppmastery
+    ~ScopedTimer() {
+        // TODO: calculate duration and print
+    }
+
+private:
+    const char* name_;
+    std::chrono::time_point<std::chrono::high_resolution_clock> start_;
+};
+
+}  // namespace moderncppmastery
