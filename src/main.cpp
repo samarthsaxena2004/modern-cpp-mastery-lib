0d3b7cbc2494cc4a43ecd5428f398cfecef9fb01
Samarth Saxena <samarthsaxena001@gmail.com>
2026-03-17 18:38:00 +0530

    add demo main.cpp with FixedVector and move semantics test

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
+#include "moderncppmastery/fixed_vector.hpp"
+#include "moderncppmastery/raii_helpers.hpp"
 #include <iostream>
 
 int main() {
-    // TODO: add demo code
+    moderncppmastery::ScopedTimer timer("Entire demo");
+
+    moderncppmastery::FixedVector<int, 10> fv;
+    fv.push_back(42);
+    fv.push_back(100);
+
+    std::cout << "FixedVector size: " << fv.size() << "\n";
+
+    // Move semantics demo
+    auto fv2 = std::move(fv);
+    std::cout << "After move, fv2 size: " << fv2.size() << "\n";
+
     return 0;
 }
